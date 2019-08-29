b03718759727302bb1938dc2fd76886603ad9f56
xusiwei11 <54676131+xusiwei11@users.noreply.github.com>
2019-08-29 10:11:10 -0500

    Hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: SIwei XU
+ * Date: 2017/8/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Philip Chohon and Siwei XU\n");
+  printf("computer Science\n")
+  return 0;
+}
